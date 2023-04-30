e5da7613e55fc3fcdf0c01c5ec353829f9c67a06
Ericlesp <ericlesrb@academico.ufs.br>
2023-04-30 10:45:15 -0300

    endpoint de adicionar curso

--- app/Http/Controllers/CursoRegistrarController.php
+++ app/Http/Controllers/CursoRegistrarController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Curso;
+use App\Curso;
 use Illuminate\Http\Request;
 
 class CursoRegistrarController extends Controller
 {
-    private Curso $curso;
+    public function adiciona(
+        Request $request
+    ) {
+        $dadosCurso = $request->validate([
+            'nome' => ['required', 'string', 'max:30'],
+            'descricao' => ['nullable', 'string', 'max:60'],
+            'codigo' => ['required', 'string', 'max:50'],
+            'valor' => ['required', 'numeric', 'regex:/^\d+(\.\d{1,2})?$/']
+        ]);
 
-    public function __invoke(CursoRegistrarRequest $cursosRegistrarRequest) {}
+        $curso = new Curso();
+        $curso->nome = $dadosCurso['nome'];
+        $curso->descricao = $dadosCurso['descricao'];
+        $curso->codigo = $dadosCurso['codigo'];
+        $curso->valor = $dadosCurso['valor'];
+
+        $curso->save();
+
+        return response("", 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AlunoConsultaController;
 use App\Http\Controllers\AlunoDeletaController;
 use App\Http\Controllers\AlunoEditaController;
 use App\Http\Controllers\AlunoRegistrarController;
+use App\Http\Controllers\CursoRegistrarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,5 @@ Route::get('/alunos', [AlunoConsultaController::class, 'consulta']);
 Route::delete('/alunos/{alunoId}', [AlunoDeletaController::class, 'deleta']);
 
 Route::post('/alunos/{alunoId}', [AlunoEditaController::class, 'edita']);
+
+Route::post('/cursos', [CursoRegistrarController::class, 'adiciona']);
